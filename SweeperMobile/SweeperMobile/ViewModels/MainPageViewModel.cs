30f32316929e7f7e2b3468fbe6336b2b7b316444
bicyclemark <mark.d.wardell@gmail.com>
2020-06-28 21:15:06 -0500

    Added the MAanPageViewModel

---
+++ SweeperMobile/SweeperMobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace SweeperMobile.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+       // public ObservableCollection<GamePiece> Board { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
